b720bff57b6ea91e81e4258a9e27597e8fd7647a
Joseph Tatum <josephtatum@gmail.com>
2019-10-29 10:23:48 -0700

    built out filter by keyword functionality

--- home/app.js
+++ home/app.js
@@ -3,12 +3,36 @@ import htmlToDOM from '../util/html-to-DOM.js';
 import { renderImage } from '../util/render-image.js';
 
 const photoGalleryUl = document.getElementById('photo-gallery');
+const selectInput = document.getElementById('selection-menu');
 
 images.forEach(image => {
     const renderedImage = renderImage(image);
     const dom = htmlToDOM(renderedImage);
-    console.log(dom);
 
     photoGalleryUl.appendChild(dom);
+});
+
+selectInput.addEventListener('change', () => {
+    while(photoGalleryUl.lastElementChild) {
+        photoGalleryUl.lastElementChild.remove();
+    }
+    const userSelectedInput = selectInput.value;
+    let filteredArray = null;
+
+    filteredArray = images.filter(image => image.keyword === userSelectedInput);
+    
+    if(filteredArray.length === 0) {
+        filteredArray = images;
+    }
+    
+    filteredArray.forEach(image => {
+        const renderedImage = renderImage(image);
+        const dom = htmlToDOM(renderedImage);
+        console.log(dom)
+        photoGalleryUl.appendChild(dom);
+    });
+
+    
+
 });
 
