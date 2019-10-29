d9181154ba91012ee0ce52426c8bf9cad0fbd33f
Joseph Tatum <josephtatum@gmail.com>
2019-10-29 09:38:52 -0700

    created passing test for renderImage()

--- test/tests.js
+++ test/tests.js
@@ -1,2 +1,31 @@
 import './html-equal.js';
-// add your test files:
+import { renderImage } from '../util/render-image.js';
+const test = QUnit.test;
+
+QUnit.module('Render Image');
+
+test('Renders HTML from data', assert => {
+    // arrange
+    const narwhal = {
+        url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+        title: 'UniWhal',
+        description: 'A unicorn and a narwhal nuzzling their horns',
+        keyword: 'narwhal',
+        horns: 1
+    };
+
+    const expected = /*html*/`
+        <li class="gallery-item" style="background-image: url('http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg')">
+            <div class="item-description">
+                <h2 class="image-title">UniWhal</h2>
+                <p>Horns: 1</p>
+            </div>
+        </li>
+    `;
+
+    // act
+    const html = renderImage(narwhal);
+
+    // assert
+    assert.htmlEqual(html, expected);
+});
